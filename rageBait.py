9a2389c6941d99bd18a5d5c81a9456863c7cfba1
Lacomus36 <Jamesamusick@gmail.com>
2026-02-09 03:33:24 -0500

    Patch 0.1

--- rageBait.py
+++ rageBait.py
@@ -1,6 +1,7 @@
 #Created 2/2/2026 11:02PM
 #Original Coder James Musick
 #In this code you should find game code for RageBait, CS450 Capstone project Team Joy
+#VERSION 0.1
 import pygame
 import json
 
